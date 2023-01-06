2dbc0ad9654a361dfd2e622862d748982e65a426
priya9soni <117305478+priya9soni@users.noreply.github.com>
2023-01-06 18:32:30 +0530

    Update Assi1_testurl_chrome.java

--- assignment/src/assignment_module2/Assi1_testurl_chrome.java
+++ assignment/src/assignment_module2/Assi1_testurl_chrome.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Assi1_testurl_chrome {
 
 	public static void main(String[] args) throws InterruptedException {
-		
+		// This is test comment
 		/*
 		 * System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe"); WebDriver
 		 * drive = new FirefoxDriver(); drive.get("https://Google.com");
